4b0047a45670ecebee61e4db951880914bb5e3f4
Fernando Flores <floresf82@gmail.com>
2016-10-19 11:15:19 -0300

    Fixed issues with characters (encoding) and format

--- main.R
+++ main.R
@@ -7,33 +7,46 @@ source('create_matrix.R')
 source('classify_emotion.R')
 
 corpus2 <- read.csv("data/StemCorpus.csv")
+
 # classify emotion
-class_emo = classify_emotion(corpus2$text, algorithm="bayes", prior=1.0)
+class_emo <- 
+    classify_emotion(corpus2$text, algorithm = "bayes", prior = 1.0)
+
 # get emotion best fit
- emotion = class_emo[,7]
+emotion <- class_emo[, 7]
+
 # substitute NA's by "unknown"
-emotion[is.na(emotion)] = "unknown"
+emotion[is.na(emotion)] <- "unknown"
 
 # classify polarity
-class_pol = classify_polarity(corpus2$text, algorithm="bayes", minWordLength = 1)
+class_pol <- 
+    classify_polarity(corpus2$text, algorithm = "bayes", minWordLength = 1)
 
 
 # get polarity best fit
- polarity = class_pol[,4]
+polarity <- class_pol[, 4]
 
-#PROBANDO C?DIGO DE NAIVE BAYES
- sent_df = data.frame(text=corpus2, emotion=emotion,polarity=polarity, stringsAsFactors=FALSE)
+#PROBANDO CÓDIGO DE NAIVE BAYES
+sent_df <- data.frame(text = corpus2, 
+                      emotion = emotion,
+                      polarity = polarity, 
+                      stringsAsFactors = FALSE)
 
- #sort data frame
-sent_df = within(sent_df,
-emotion <- factor(emotion, levels=names(sort(table(emotion), decreasing=TRUE))))
+#sort data frame
+sent_df <- 
+    within(sent_df,
+           emotion <- factor(emotion, 
+                             levels = names(sort(table(emotion), 
+                                                 decreasing = TRUE))))
 
 
 # plot distribution of polarity
-ggplot(sent_df, aes(x=polarity)) +
-  geom_bar(aes(y=..count.., fill=polarity)) +
-  scale_fill_brewer(palette="RdGy") +
-  labs(x="CategorÃ­as", y="Comentarios") +
-  
-  labs(title = "AnÃ¡lisis de Emociones para la mejora del docente\n(ClasificaciÃ³n de comentarios por polaridad)",
-       plot.title = element_text(size=12))
+ggplot(sent_df, 
+       aes(x = polarity)) +
+    geom_bar(aes(y = ..count.., fill = polarity)) +
+    scale_fill_brewer(palette = "RdGy") +
+    labs(x = "Categorías", y = "Comentarios") +
+    labs(title = 
+             paste0("Análisis de Emociones para la mejora del docente",
+                    "\n(Clasificación de comentarios por polaridad)"),
+         plot.title = element_text(size = 12))
